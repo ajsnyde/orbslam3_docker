6cd89241cb025f6a03012d4ed238bb15ebc61236
Addison Snyder <addison@addisonsnyder.com>
2023-02-15 14:18:43 -0500

    added new volumes, renamed directories, added super basic camera config

--- build_container_cuda.sh
+++ build_container_cuda.sh
@@ -25,7 +25,7 @@ docker pull jahaniam/orbslam3:ubuntu18_melodic_cuda
 
 # Remove existing container
 docker rm -f orbslam3 &>/dev/null
-[ -d "ORB_SLAM3" ] && sudo rm -rf ORB_SLAM3 && mkdir ORB_SLAM3
+#[ -d "ORB_SLAM3" ] && sudo rm -rf ORB_SLAM3 && mkdir ORB_SLAM3
 
 # Create a new container
 docker run -td --privileged --net=host --ipc=host \
@@ -40,15 +40,16 @@ docker run -td --privileged --net=host --ipc=host \
     -v `pwd`/Datasets:/Datasets \
     -v /etc/group:/etc/group:ro \
     -v `pwd`/ORB_SLAM3:/ORB_SLAM3 \
+    -v `pwd`/inputFiles:/inputFiles \
     jahaniam/orbslam3:ubuntu18_melodic_cuda bash
 
+
+docker exec -it orbslam3 bash -i -c "if cd ORB_SLAM3; then git pull; else git clone -b master https://github.com/ajsnyde/ORB_SLAM3 /ORB_SLAM3 && cd /ORB_SLAM3; fi"
 # Git pull orbslam and compile
-docker exec -it orbslam3 bash -i -c "git clone -b master https://github.com/ajsnyde/ORB_SLAM3 /ORB_SLAM3 && cd /ORB_SLAM3 && chmod +x build.sh && ./build.sh "
+docker exec -it orbslam3 bash -i -c "chmod +x build.sh && ./build.sh"
 # Compile ORBSLAM3-ROS
 docker exec -it orbslam3 bash -i -c "echo 'ROS_PACKAGE_PATH=/opt/ros/melodic/share:/ORB_SLAM3/Examples/ROS'>>~/.bashrc && source ~/.bashrc && cd /ORB_SLAM3 && chmod +x build_ros.sh && ./build_ros.sh"
 
 chmod -R 777  ORB_SLAM3/
-
-cp -a shared/. ORB_SLAM3/
 
 docker exec -it orbslam3 bash
