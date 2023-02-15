d88a0f6dbc6dc4a83f9ddca166b85ee63e9c0bfa
Addison Snyder <addison@addisonsnyder.com>
2023-02-15 15:03:11 -0500

    fixed downsize script

--- inputFiles/downsize.sh
+++ inputFiles/downsize.sh
@@ -1,1 +1,1 @@
-for i in raw/*; do ffmpeg -i -n "$i" -vf scale=720:-1 "downsized/$(basename ${i%.*}).mp4"; done
+for i in raw/*; do ffmpeg -n -i "$i" -vf scale=720:-1 "downsized/$(basename ${i%.*}).mp4"; done
